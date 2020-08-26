59cb8b7b4fbf7bd19897a2046da8bc0ea7a35a20
Savinov Danil <savinov.danil18@outlook.com>
2020-08-26 17:51:43 +0300

    messages

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='https://saletur.ru/galery/tfoto/big/043/73/437321.jpg'></img>
+			</div>
+			<div>
+				ava + discription
+        	</div>
+			<div>
+				my post
+          		<div>
+					New post;
+          		<div>
+						<div>
+							post1
+         			</div>
+						<div>
+							post2
+          			</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
