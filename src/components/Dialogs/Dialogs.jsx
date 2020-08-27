813459e80ccdadca8674cb11d58bc79ab2da556a
Savinov Danil <savinov.danil18@outlook.com>
2020-08-27 19:06:37 +0300

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import s from './Dialogs.modul.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog}>
+					Danich
+				</div>
+				<div className={s.dialog}>
+					Andrey
+				</div>
+				<div className={s.dialog}>
+					Vadik
+				</div>
+				<div className={s.dialog}>
+					Eleonora
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>How is you job</div>
+				<div className={s.message}>OO</div>
+			</div>
 		</div>
 	)
 }
